1a97b1867877f128b1ce366ab8f69c73b21a2847
Marcus Jefferson <marcus.jefferson@porsche.digital>
2026-06-02 12:55:06 -0700

    finished remove nth node

--- src/linked_list/remove_nth_node_from_list.rs
+++ src/linked_list/remove_nth_node_from_list.rs
@@ -3,6 +3,8 @@ use crate::linked_list::utils::*;
 struct Solution {}
 impl Solution {
     pub fn remove_nth_from_end(head: Option<Box<ListNode>>, n: i32) -> Option<Box<ListNode>> {
+        let mut head = head;
+
         let mut cur = head.as_ref();
         let mut list_len: i32 = 0;
 
@@ -11,9 +13,29 @@ impl Solution {
             list_len += 1;
         }
 
-        println!("{:?}", list_len);
+        // removal cases
+        // 1. nth node at the start of the list
+        // 2. nth node has a next that is not none
+        // 3. nth node is the end of the list
 
-        todo!()
+        let target = list_len - n;
+
+        if target == 0 {
+            return head.unwrap().next;
+        }
+
+        // println!("{:?}", list_len);
+        let mut left = head.as_mut().unwrap();
+
+        // stop in front of the nth node to remove
+        for _ in 0..(list_len - n - 1) {
+            left = left.next.as_mut().unwrap()
+        }
+
+        let removed = left.next.take();
+        left.next = removed.unwrap().next;
+
+        head
     }
 }
 
